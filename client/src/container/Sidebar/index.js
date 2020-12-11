7bdf3152d1bf1c7b5e14a7fc23c06d3a678f0114
Ahmadjon <info.ahmadjon@gmail.com>
2020-12-11 14:40:25 +0500

    responsive enabled

--- client/src/container/Sidebar/index.js
+++ client/src/container/Sidebar/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
 import User from "./User";
 import "./style.css";
+import axios from "../../utils/axios";
 
 const Contact = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios.get("/users").then((res) => {
+      setUsers(res.data);
+    });
+  }, []);
+  console.log(users);
   return (
     <div className="contact">
       <div className="contact__header">
